d02a52ea7e1f68b0fc1947a8dd826e689a96ccaa
corvinus-i2ifld <robert.obertol@stud.uni-corvinus.hu>
2022-09-30 10:42:44 +0200

    ermoving first name

--- UserMaintanence/UserMaintanence/Entitites/User.cs
+++ UserMaintanence/UserMaintanence/Entitites/User.cs
@@ -1,25 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace UserMaintanence.Entitites
 {
     class User
     {
         public Guid ID { get; set; } = Guid.NewGuid();
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string FullName
-        {
-            get
-            {
-                return string.Format(
-                    "{0} {1}",
-                    LastName,
-                    FirstName);
-            }
+        public string FullName{get; set;}
         }
     }
-}
--- UserMaintanence/UserMaintanence/Form1.cs
+++ UserMaintanence/UserMaintanence/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using UserMaintanence.Entitites;
 
@@ -19,7 +13,6 @@ namespace UserMaintanence
         {
             InitializeComponent();
             lblLastName.Text = Resource1.LastName;
-            lblFirstName.Text = Resource1.FirstName;
             btnAdd.Text = Resource1.Add;
             listUsers.DataSource = users;
             listUsers.ValueMember = "ID";
@@ -30,8 +23,7 @@ namespace UserMaintanence
         {
             var u = new User()
             {
-                LastName = txtLastName.Text,
-                FirstName = txtFirstName.Text
+                FullName = txtLastName.Text,              
             };
             users.Add(u);
         }

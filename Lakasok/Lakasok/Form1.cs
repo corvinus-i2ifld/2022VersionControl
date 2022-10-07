052f3ad118312e92465500170a2e117a5d9f4d48
corvinus-i2ifld <robert.obertol@stud.uni-corvinus.hu>
2022-10-07 10:44:49 +0200

    Exel behivatkozva

--- Lakasok/Lakasok/Form1.cs
+++ Lakasok/Lakasok/Form1.cs
@@ -7,14 +7,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 namespace Lakasok
 {
     public partial class Form1 : Form
     {
+       
+        RealEstateEntities1 context = new RealEstateEntities1();
+        List<Flat> Flats;
+        Excel.Application xlApp; 
+        Excel.Workbook xlWB; 
+        Excel.Worksheet xlSheet; 
+
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            Flats = context.Flat.ToList();
         }
     }
 }

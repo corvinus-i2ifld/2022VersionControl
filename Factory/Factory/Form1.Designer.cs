37b8afec71999bbee6bd1c0af43b421c8de5f586
corvinus-i2ifld <robert.obertol@stud.uni-corvinus.hu>
2022-11-04 11:17:58 +0100

    Color button

--- Factory/Factory/Form1.Designer.cs
+++ Factory/Factory/Form1.Designer.cs
@@ -36,6 +36,7 @@ namespace Factory
             this.btnSelect_Car = new System.Windows.Forms.Button();
             this.btnSelect_Ball = new System.Windows.Forms.Button();
             this.lblNext = new System.Windows.Forms.Label();
+            this.btnColor = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // createTimer
@@ -86,11 +87,21 @@ namespace Factory
             this.lblNext.TabIndex = 3;
             this.lblNext.Text = "Coming Next:";
             // 
+            // btnColor
+            // 
+            this.btnColor.Location = new System.Drawing.Point(126, 52);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(75, 23);
+            this.btnColor.TabIndex = 4;
+            this.btnColor.UseVisualStyleBackColor = true;
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnColor);
             this.Controls.Add(this.lblNext);
             this.Controls.Add(this.btnSelect_Ball);
             this.Controls.Add(this.btnSelect_Car);
@@ -110,6 +121,7 @@ namespace Factory
         private System.Windows.Forms.Button btnSelect_Car;
         private System.Windows.Forms.Button btnSelect_Ball;
         private System.Windows.Forms.Label lblNext;
+        private System.Windows.Forms.Button btnColor;
     }
 }
 

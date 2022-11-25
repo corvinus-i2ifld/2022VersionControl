a32ba78b2a75fb8dd5924ffd56447afe34f9e29c
corvinus-i2ifld <robert.obertol@stud.uni-corvinus.hu>
2022-11-25 10:21:03 +0100

    E-mail validálás teszt függvény

---
+++ UnitTestExample/UnitTestExample/AccountControllerTestFixture.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestExample
+{
+    public class AccountControllerTestFixture
+    {
+        public void TestValidateEmail(string email, bool expectedResult)
+        {
+
+        }
+    }
+}

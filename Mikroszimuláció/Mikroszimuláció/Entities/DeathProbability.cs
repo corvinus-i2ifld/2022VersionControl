192b1a32c91e2f8bf9ce12d8c0baa661cb7606d8
corvinus-i2ifld <robert.obertol@stud.uni-corvinus.hu>
2022-12-02 09:46:22 +0100

    Osztályok létrehozása

---
+++ Mikroszimuláció/Mikroszimuláció/Entities/DeathProbability.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mikroszimuláció.Entities
+{
+    public class DeathProbability
+    {
+
+        public int BirthYear { get; set; }
+        public Gender Gender { get; set; }
+        public double deathProbability { get; set; }
+
+    }
+}
